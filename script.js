392e67f84e848b8227bb3ed9b63b03d816490c33
JohnEsperas <119088209+JohnEsperas@users.noreply.github.com>
2023-02-04 12:31:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', function() {
+  window.location.href = "add.html";
+});
+
+
+const btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", function() {
+  window.location.href = "todo.html";
+});
+
+
+const btn3 = document.getElementById("btn3");
+btn3.addEventListener("click", function() {
+  window.location.href = "form.html";
+});
+
+
+const btn4 = document.getElementById("btn4");
+btn4.addEventListener("click", function() {
+  window.location.href = "array.html";
+});
+
+const btn5 = document.getElementById("btn5");
+btn5.addEventListener("click", function() {
+  window.location.href = "string.html";
+});
+
+
+const btn6 = document.getElementById("btn6");
+btn6.addEventListener("click", function() {
+  window.location.href = "condition.html";
+});
+
+
+const btn7 = document.getElementById("btn7");
+btn7.addEventListener("click", function() {
+  window.location.href = "currency.html";
+});
+
+
+const btn8 = document.getElementById("btn8");
+btn8.addEventListener("click", function() {
+  window.location.href = "calculator.html";
+});
+
